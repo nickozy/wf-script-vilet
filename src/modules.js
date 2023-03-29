41695172f5cbef5813b692106210bd758df5982b
nickozy <104327959+nickozy@users.noreply.github.com>
2023-03-30 02:27:58 +0300

    cookie module add

--- src/modules.js
+++ src/modules.js
@@ -3,3 +3,4 @@ export { default as Scroll } from "./modules/Scroll";
 export { default as Scrollbar } from "./modules/Scrollbar";
 export { default as Nav } from "./modules/Nav";
 export { default as Filter } from "./modules/Filter";
+export { default as Cookie } from "./modules/Cookie";
---
+++ src/modules/Cookie.js
@@ -0,0 +1,16 @@
+import { module } from "modujs";
+
+export default class extends module {
+  constructor(m) {
+    super(m);
+    this.events = {
+        click: {
+          close: 'closeCookie',
+        }
+      }
+  }
+
+  init() {
+    
+  }  
+}
