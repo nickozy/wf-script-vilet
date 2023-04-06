7d917ec90f38d419ae4ee2dddbe1a5816ee06fb2
nickozy <104327959+nickozy@users.noreply.github.com>
2023-04-06 16:14:44 +0300

    video main

--- src/modules.js
+++ src/modules.js
@@ -5,3 +5,4 @@ export { default as Nav } from "./modules/Nav";
 export { default as Filter } from "./modules/Filter";
 export { default as Cookie } from "./modules/Cookie";
 export { default as Video } from "./modules/Video";
+export { default as Videomain } from "./modules/Videomain";
---
+++ src/modules/Videomain.js
@@ -0,0 +1,13 @@
+import { module } from 'modujs';
+
+export default class extends module {
+    constructor(m) {
+        super(m);
+    }
+    init(){
+        const el = this.el
+    if(window.innerWidth < 767){
+        el.setAttribute("src", "https://data.d.starterapp.co/api/v1/buckets/breadhead/objects/download?preview=true&prefix=dmlsZXQgb2sgQk9STiBBRi0zMDAwLm1wNA==&version_id=null")
+    }
+    }
+}  
